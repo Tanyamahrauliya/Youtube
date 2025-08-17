4830a51d6d1788326ace787c0a4c8ce4676ce7f9
Tanya Mahrauliya <cashbread007@gmail.com>
2025-08-17 12:31:10 +0530

    first commit

---
+++ program.py
@@ -0,0 +1,6 @@
+u= int(input("Enter a number (-1 to stop ):"))
+while u !=-1:
+    print(f"your entered number :{u}")
+    u = int(input("Enter a number (-1 to stop):"))
+print("loop exite ! Thank You")
+
