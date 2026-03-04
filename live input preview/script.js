5bd2eafcc22ee617dbaaac6918ea9863f82d8945
Tanya Mahrauliya <cashbread007@gmail.com>
2026-03-04 20:55:08 +0530

    live input preview

--- live input preview/script.js
+++
@@ -1,6 +0,0 @@
-let input = document.getElementById("textInput");
-let preview = document.getElementById("preview");
-
-input.addEventListener("input", function () {
-  preview.innerText = input.value;
-});
